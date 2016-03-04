ee6ab9f75fa460dfcdc0e00b954ecfc16dc9962a
Nicolas Grellety <nicolas.grellety@protonmail.com>
2016-03-04 21:43:04 +0100

    nb_users sur général plus verif user connected

--- sitecollaboratif/app/Controller/ChatsController.php
+++ sitecollaboratif/app/Controller/ChatsController.php
@@ -3,11 +3,26 @@
 		var $name = 'Chats';
 
 		public function index() {
+			if (!$this->Auth->user('id')) {
+				throw new NotFoundException();
+			}
+			
 			$this->layout = 'default2';
 
 			$room = $this->Chat->Rooms->find('all', array(
 		  			'fields'=>array('id', 'name')
 		  	));
+
+			// on va rechercher la salle de tchat "général" pour augmenter le nombre d'utilisateurs connectés
+		  	$general = $this->Chat->Rooms->find('first', array(
+		  		'conditions'=>array('Rooms.name' => 'Général'),
+		  		'fields'=>array('id', 'nb_users')
+		  	));
+
+		  	$nb_connected = $general['Rooms']['nb_users'] + 1;
+
+		  	$this->Chat->Rooms->id = $general['Rooms']['id'];
+		  	$this->Chat->Rooms->saveField('nb_users', $nb_connected);
 
 		  	$this->set('rooms', $room);
 		}
